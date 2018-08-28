f72d9d2270ca7d943b6f35c008d55648e2bd1e93
Chi Chen <mars97025@gmail.com>
2018-08-28 18:54:40 +0800

    website revision

---
+++ js/jq-gotop.js
@@ -0,0 +1,18 @@
+// 超简洁代码 jQuery goTop(返回顶部)
+$(function() {
+  var $goTop = $('<div id="goTop" style="border-radius:50%;background:#333;color:#fff;text-align:center;padding:10px 13px 7px 13px;position:fixed;bottom:10px;right:10px;cursor:pointer;display:none;font-family:verdana;font-size:22px;">^</div>').appendTo('body');
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() != 0) {
+      $goTop.fadeIn();
+    } else {
+      $goTop.fadeOut();
+    }
+  });
+
+  $goTop.click(function() {
+    $('body, html').animate({
+      scrollTop: 0
+    }, 200);
+  });
+});
