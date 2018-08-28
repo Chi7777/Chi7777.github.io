10a834efcbee2e771188ba5eb606e3e7db1ba1c1
Chi Chen <mars97025@gmail.com>
2018-08-28 20:09:37 +0800

    modify arrow

--- js/jq-gotop.js
+++ js/jq-gotop.js
@@ -1,6 +1,6 @@
 // 超简洁代码 jQuery goTop(返回顶部)
 $(function() {
-  var $goTop = $('<div id="goTop" style="border-radius:50%;background:#333;color:#fff;text-align:center;padding:10px 13px 7px 13px;position:fixed;bottom:10px;right:10px;cursor:pointer;display:none;font-family:verdana;font-size:22px;">^</div>').appendTo('body');
+  var $goTop = $('<div id="goTop" style="border-radius:50%;background:#333;color:#fff;text-align:center;padding:10px 13px 7px 13px;position:fixed;bottom:10px;right:10px;cursor:pointer;display:none;font-size:22px;"><i class="fal fa-angle-up"></i></div>').appendTo('body');
 
   $(window).scroll(function() {
     if ($(this).scrollTop() != 0) {
